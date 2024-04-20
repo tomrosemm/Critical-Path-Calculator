2e3f63904349fa76a266797c596fe37d690aa6a1
tomrosemm <tomrose@umich.edu>
2024-04-20 14:38:36 -0400

    Delete row button implementation

--- mainScript.js
+++ mainScript.js
@@ -26,6 +26,14 @@ window.onclick = function(event) {
   }
 }
 
+// Delete Row Button
+
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("myTable").deleteRow(i);
+}
+
+
 
 // IDK if it's obvious and I'm slow but I think using the clicking of the predecessor & successor dropdown menus as the 
 // trigger to gather the appropriate elements to populate the menu would be a good methodology
