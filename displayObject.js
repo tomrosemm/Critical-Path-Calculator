89d6d00876aca6cc8421133cd5b01fdf7c1a3b9b
tomrosemm <tomrose@umich.edu>
2024-04-21 17:36:47 -0400

    setup2

---
+++ displayObject.js
@@ -0,0 +1,14 @@
+
+
+// Displaying object properties
+
+export const tableInputRow = {
+    name: "Task 1",
+    predecessors: "Task 0",
+    successors: "Task 2",
+    duration: 5
+};
+
+// Use Object.values to convert the object to an array and display it easily
+const tableInputAsArray = Object.values(tableInputRow)
+document.getElementById("outputBox").innerHTML = tableInputAsArray
