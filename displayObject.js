5c85b7173705c714652e2b8a369338bf6d6f5f30
tomrosemm <tomrose@umich.edu>
2024-04-21 20:27:22 -0400

    using an array to hold and access the display object

--- displayObject.js
+++ displayObject.js
@@ -1,12 +1,16 @@
-// Displaying object properties
+// Define rowArray and push tableInputRow into it
+const rowArray = [];
 
-export const tableInputRow = {
+const tableInputRow = {
     name: "Task 1",
     predecessors: "Task 0",
     successors: "Task 2",
     duration: 5
 };
 
+rowArray.push(tableInputRow);
+
 // Use Object.values to convert the object to an array and display it easily
-const tableInputAsArray = Object.values(tableInputRow)
-document.getElementById("outputBox").innerHTML = tableInputAsArray
+const tableInputAsArray = Object.values(tableInputRow);
+
+document.getElementById("outputBox").innerHTML = Object.values(rowArray[0])
