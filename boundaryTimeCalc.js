159f29a2902c4c644c725ec49e7bae162464b070
jarum3 <117324735+jarum3@users.noreply.github.com>
2024-04-22 22:10:24 -0400

    EST and EFT functions

--- boundaryTimeCalc.js
+++ boundaryTimeCalc.js
@@ -6,9 +6,22 @@ function calcestandeft(arr) {
 }
 
 function calcest(arr, index) {
+  // Set initial activity est to 0
   if (index == 0) {
     arr[index].est = 0;
+    return;
   }
+  let max = -9999;
+  // Loop through each other task
+  for (let j = 0; j <= arr.length; j++) {
+    // If our task has that task as a predecessor
+    if (arr[index].predecessors.includes(arr[j].name)) {
+      if (arr[j].eft > maximum) { // Set the new EST to that task's EFT
+        maximum = arr[j].eft;
+      // If we have multiple predecessors, set the task to the maximum predecessor EFT
+      }
+    }
+  } 
 }
 
 function calceft(arr, index) {
