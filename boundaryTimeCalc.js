8b72c529b77f8df2409cf92bb39fbd4adcb93f0b
jarum3 <117324735+jarum3@users.noreply.github.com>
2024-04-23 01:14:14 -0400

    Fixing typo

--- boundaryTimeCalc.js
+++ boundaryTimeCalc.js
@@ -53,7 +53,7 @@ function calclst(arr, index) {
 }
 
 function calclft(arr, index) {
-  // Set final activity est to be the EFT of the final task
+  // Set final activity LFT to be the EFT of the final task
   if (index == 0) {
     arr[index].lft = arr[index].eft;
     return;
